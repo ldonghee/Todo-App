0fb4e750c98e41482b94a4a9ce69075ca9fa19c2
Donghui-Lee <ehdgml3206@naver.com>
2020-02-06 23:37:15 +0900

    [2020.02.06] 작업내용 - 대량의 데이터 렌더링 - useMemo 적용(컴포넌트 성능 최적화) - useState 함수형 업데이트(함수 생성 성능 최적화)

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+
+function createBulkTodos() {
+  const array = [];
+  for(let i=1; i<=2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false
+    });
+  }
+  return array;
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리엑트 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링해 보기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "일정 고나리 앱 만들어 보기",
-      checked: false,
-    }
-  ]);
+
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: "리엑트 기초 알아보기",
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: "컴포넌트 스타일링해 보기",
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: "일정 고나리 앱 만들어 보기",
+  //     checked: false,
+  //   }
+  // ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고유값으로 사용할 id
   // ref 사용
@@ -32,17 +47,17 @@ const App = () => {
       text,
       checked: false
     };
-    setTodos(todos.concat(todo));
+    setTodos(todos => todos.concat(todo));
     nextId.current += 1;
-  }, [todos]);
+  }, []);
 
   const onRemove = useCallback((id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }, [todos]);
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, []);
 
   const onToggle = useCallback((id) => {
-    setTodos(todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo));
-  }, [todos]);
+    setTodos(todos => todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo));
+  }, []);
 
   return (
     <TodoTemplate>
